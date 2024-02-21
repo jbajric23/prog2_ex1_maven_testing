ba5a3231fcce44b76803bcf67c2fe93ecbd8ac10
JASMINS_PROBOOK\Jasmi <jasmin.bajric23@outlook.com>
2024-02-21 20:26:20 +0100

    update documention HomeControllerTest

--- src/main/test/at/ac/fhcampuswien/fhmdb/HomeControllerTest.java
+++ src/main/test/at/ac/fhcampuswien/fhmdb/HomeControllerTest.java
@@ -3,5 +3,5 @@ package at.ac.fhcampuswien.fhmdb;
 import static org.junit.jupiter.api.Assertions.*;
 
 class HomeControllerTest {
-
+    // TODO implement unittests for the HomeController class -> TDD
 }
