5e138eabad689b8d3d7e7fef0c7407b0999004fd
Jasmin Bajric <jasmin.bajric@stud.fh-campuswien.ac.at>
2024-02-26 15:37:33 +0100

    implement new MovieTest class

--- src/main/test/at/ac/fhcampuswien/fhmdb/models/MovieTest.java
+++ src/main/test/at/ac/fhcampuswien/fhmdb/models/MovieTest.java
@@ -1,4 +1,22 @@
+package at.ac.fhcampuswien.fhmdb.models;
+
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class MovieTest {
-  
+
+    // TODO implement unittests for the Movie class -> TDD
+    
+    @Test
+    void getTitle() {
+    }
+
+    @Test
+    void getDescription() {
+    }
+
+    @Test
+    void initializeMovies() {
+    }
 }
