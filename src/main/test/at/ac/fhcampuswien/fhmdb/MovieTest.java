6a3d01a63b07cd61203c46c767e59c85d98a1b39
Jasmin Bajric <jasmin.bajric@stud.fh-campuswien.ac.at>
2024-02-26 15:37:09 +0100

    Remove old testing class

--- src/main/test/at/ac/fhcampuswien/fhmdb/MovieTest.java
+++
@@ -1,8 +0,0 @@
-package at.ac.fhcampuswien.fhmdb;
-
-import static org.junit.jupiter.api.Assertions.*;
-
-public class MovieTest {
-    // TODO implement unittests for Movie class -> TDD
-    // Check for Push
-}
