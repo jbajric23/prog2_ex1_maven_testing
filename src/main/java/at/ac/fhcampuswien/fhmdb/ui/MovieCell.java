ee8c016a14b3ae16c89074be51a3d61265825987
Jasmin Bajric <jasmin.bajric@stud.fh-campuswien.ac.at>
2024-03-08 20:24:21 +0100

    Implement Genres to MovieCell with new Layout

--- src/main/java/at/ac/fhcampuswien/fhmdb/ui/MovieCell.java
+++ src/main/java/at/ac/fhcampuswien/fhmdb/ui/MovieCell.java
@@ -12,7 +12,8 @@ import javafx.scene.paint.Color;
 public class MovieCell extends ListCell<Movie> {
     private final Label title = new Label();
     private final Label detail = new Label();
-    private final VBox layout = new VBox(title, detail);
+    private final Label genres = new Label();
+    private final VBox layout = new VBox(title, detail, genres);
 
     @Override
     protected void updateItem(Movie movie, boolean empty) {
@@ -20,6 +21,7 @@ public class MovieCell extends ListCell<Movie> {
 
         if (empty || movie == null) {
             setText(null);
+            setGraphic(null);
         } else {
             this.getStyleClass().add("movie-cell");
             title.setText(movie.getTitle());
@@ -28,11 +30,23 @@ public class MovieCell extends ListCell<Movie> {
                             ? movie.getDescription()
                             : "No description available"
             );
-
+            genres.setText(
+                    movie.getGenres() != null
+                            ? movie.getGenres().toString()
+                            : "No genres available"
+            );
 
             // color scheme
             title.getStyleClass().add("text-yellow");
             detail.getStyleClass().add("text-white");
+            // Make the genres text display in white
+            genres.getStyleClass().add("text-white");
+            // Remove the brackets from the genres text
+            String genresText = genres.getText();
+            genresText = genresText.replace("[", "");
+            genresText = genresText.replace("]", "");
+            genres.setText(genresText);
+
             layout.setBackground(new Background(new BackgroundFill(Color.web("#454545"), null, null)));
 
             // layout
